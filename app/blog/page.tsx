638fc7e215c7595c8750f39648b26aea26a6496b
unknown <mahamuduhalic.official@gmail.com>
2026-01-22 10:07:24 +0000

    chore: blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="absolute top-1/2 left-1/2">
+      <h2 className="text-4xl">Coming Soon</h2>
+    </div>
+  );
+};
+
+export default page;
